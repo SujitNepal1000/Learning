a4629e4172670624831258e818b01255935591e9
Sujit NEPAL <sujitnepal27@gmail.com>
2024-02-21 17:25:13 +0545

    learning python basics and oop

---
+++ Basic.py
@@ -0,0 +1,152 @@
+# Car = "Ford mustang 1969"
+# name = "Jhon Wick"
+#
+# print(name + " owns " + Car)
+
+# function creation
+
+# def addition():
+#     a = 4
+#     b = 5
+#     c = int(input("enter the value of c"))
+#     print(a + b + c)
+
+
+# addition()
+
+# #arguments
+
+# def user_info(name, age, address):
+#     print("Hello i am {}, i am {} years old and i live in {}".format(name, age, address))
+
+
+# user_info("Jhon", 25, "kathmandu")
+
+
+# def greet(name):
+#     print("hello," + name + " " + "good morning")
+
+
+# greet("Jhon")
+
+# # condition
+
+# print(1 > 0)
+# print(1 <= 0)
+
+# number = int(input("enter a number"))
+# if number > 10:
+#     print("number is greater than 10")
+# elif 5 < number <= 10:
+#     print("number is less or equals to 10 and greater than 5")
+# else:
+#     print("number is less than 5")
+
+# #loops
+
+# # for loop
+
+# name = ["jhon","sam","jhonny"]
+
+# for i in name:
+#     print("Hello my name is {}".format(i))
+
+# # while loop
+
+# number = int(input("Enter any number"))
+
+#nested loop
+
+rows=int(input("enter the number of rows"))
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
+for i in range(rows,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
+
+# while number > 17:
+#     print("The range of numbers {} are".format(number))
+#     number -= 1
+#     if number == 17:
+#         break
+# for number in range (10,20):
+#     if number == 17:
+#         print("skip number 17")
+#         continue
+#     print("The numbers are {}".format(number))
+
+## list
+# fruits = [ "apple", "banana", "mango"]
+
+# print(fruits)
+
+# fruits.append("orange")
+# print(fruits)
+
+# fruits.extend(["grapes", "watermelon"])
+# print(fruits)
+
+# fruits.remove("banana")
+# print(fruits)
+
+# fruits.pop(2)
+# print(fruits)
+
+# fruits.insert(2, "kiwi") 
+# print(fruits)
+
+# number = [99,95,45,69,83]
+# number.sort()
+# print(number)
+
+# QUE find the sum of numbers in the list
+
+# def sum(num):
+#     total = 0
+#     for i in num:
+#         total += i
+#     return total
+
+# num = [1,2,3,4,5,6,7,8,9,10]
+# print("The sum of number in the list is : ",sum(num))
+
+#dictionary
+
+# student = {'name': 'John', 'age': 21,'city': 'Kathmandu'}
+# new_student = {'name': 'Sam', 'age': 22,'city': 'Lalitpur'}
+
+# print(student)
+
+# print(student['name'])
+
+# #add data to dictionary 
+# student['address'] = 'Kirtipur'
+# print(student)
+
+# #update value of a key in dictionary
+# student['age']=22
+# print(student)
+
+# #using get method
+# print(student.get('name')) 
+
+# #using items method
+# print(student.items())
+
+# #using keys method
+# print(student.keys())
+
+# #using values method
+# print(student.values())
+
+# #using pop method
+# print(student.pop('address'))
+# print(student)
+
+# student.update(new_student)
+# print(student)
+
+
---
+++ oop.py
@@ -0,0 +1,125 @@
+# # class
+
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name
+        
+#     def bark(self):
+#         print("bark bark")
+        
+#     def get_name(self):
+#         return self.name
+    
+
+# my_dog = Dog("doggy")
+# print("{} is my dog".format(my_dog.get_name()))
+# my_dog.bark()
+
+# # finding the area of rectangle creating class
+
+# class Rectangle:
+#     def __init__(self,width, height):
+#         self.width = width
+#         self.height = height
+    
+#     def area(self):
+#         return self.width * self.height
+    
+# rect1 = Rectangle(5, 4)
+# print(rect1.area())
+
+# inheritance
+
+#single inheritance
+
+# class parent:
+#     def display(self):
+#         print(" this is parent class")
+        
+# class child(parent):
+#     pass
+
+# child1 = child()
+# child1.display()
+
+#multiple inheritance
+# class parent1:
+#     def display1(self):
+#         print("this is parent1")
+
+# class parent2:
+#     def display2(self):
+#         print("this is parent2")
+        
+# class child(parent1, parent2):
+#     pass
+
+# child1 = child()
+# child1.display1()
+# child1.display2()
+
+# # multi level inheritance
+# class grandfather:
+#     def display(self):
+#         print("this is grandfather")
+        
+# class father(grandfather):
+#     def display1(self):
+#         print("this is father")
+        
+# class child(father):
+#     pass
+        
+# child1 = child()
+# child1.display()
+# child1.display1()
+
+## polymorphism
+# class animal:
+#     def __init__(self,name):
+#         self.name = name
+        
+#     def sound(self):
+#         pass
+
+# class dog(animal):
+#     def sound(self):
+#         return "woof"
+    
+# class cat(animal):
+#     def sound(self):
+#         return "meow"
+    
+# dog1 = dog("doggy")
+# cat1 = cat("kitty")
+
+# print(dog1.name)
+# print(dog1.sound())
+# print(cat1.name)
+# print(cat1.sound())
+        
+
+# que creating a class employee as a parent class and creating a subclass as a developer and manager with attributes such as name, id and salary.also calculating bonus for both the classes.
+
+class Employee:
+    def __init__(self, name, id, salary):
+        self.name = name
+        self.id = id
+        self.salary = salary
+
+    def bonus(self):
+        return self.salary * 0.10
+
+class developer(Employee):
+    def bonus(self):
+        return self.salary * 0.20
+    
+class manager(Employee):
+    def bonus(self):
+        return self.salary * 0.30
+
+dev1 = developer("John", 123456, 50000)
+mang1 = manager("Doe", 123457, 50000)
+
+print("developer bonus is: ", dev1.bonus())
+print("manager bonus is: ", mang1.bonus())
